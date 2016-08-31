d283ba438fa1c515278f4b1b85409731ec40d3e0
Leonardo Cesar Borges <leocborgess@gmail.com>
2016-08-31 06:41:27 -0300

    finish pointer example

--- pointers/pointer_test.go
+++ pointers/pointer_test.go
@@ -7,10 +7,22 @@ import (
     "github.com/leocbs/go-scratchpad/pointers"
 )
 
-func TestSumWithoutPointer(t *testing.T) {
-    value := pointers.SumValue(100)
+func TestSumPointer(t *testing.T) {
+	calculator := pointers.NewCalculatorPointer(100)
+    calculator.Sum(100)
+    value := calculator.GetMemory()
     fmt.Printf("value " , value)
-    if value == 1000{
-        t.Fatal("Pointer don't work")
+    if value != 200{
+        t.Fatal("calculator don't sum value")
+    }
+}
+
+func TestSumPerValue(t *testing.T) {
+	calculator := pointers.NewCalculatorValue(100)
+    calculator.SumNoPointer(100)
+    value := calculator.GetMemoryNoPointer()
+    fmt.Printf("value " , value)
+    if value != 100{
+        t.Fatal("calculator sum value")
     }
 }
