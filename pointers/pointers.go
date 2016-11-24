fb99c06eb7f052fffae1d967d0fcd6cbb44fbb17
Leonardo Cesar Borges <leocborgess@gmail.com>
2016-11-24 13:28:20 -0200

    print memory pointer

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,33 +1,39 @@
 package pointers
 
-type Calculator struct{
+import "fmt"
+
+type Calculator struct {
 	Memory int
 }
 
 func (calculator *Calculator) Sum(value int) {
+	fmt.Println("calculator pointer: %s", &calculator.Memory)
 	calculator.Memory = calculator.Memory + 100
 }
 
 func (calculator *Calculator) GetMemory() int {
+	fmt.Println("calculator pointer: %s", &calculator.Memory)
 	return calculator.Memory
 }
 
-func NewCalculatorPointer(initValue int) *Calculator{
+func NewCalculatorPointer(initValue int) *Calculator {
 	return &Calculator{
-		Memory:initValue,
+		Memory: initValue,
 	}
 }
 
-func NewCalculatorValue(initValue int) Calculator{
+func NewCalculatorValue(initValue int) Calculator {
 	return Calculator{
-		Memory:initValue,
+		Memory: initValue,
 	}
 }
 
 func (calculator Calculator) SumNoPointer(value int) {
+	fmt.Println("calculator pointer: %s", &calculator.Memory)
 	calculator.Memory = calculator.Memory + 100
 }
 
 func (calculator Calculator) GetMemoryNoPointer() int {
+	fmt.Println("calculator pointer: %s", &calculator.Memory)
 	return calculator.Memory
 }
